c7ab8aacf32dfdea49a5d480dee3775de8f9499a
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-07-27 12:34:36 +0530

    Time: 61 ms (98.85%), Space: 59.4 MB (53.87%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -0,0 +1,11 @@
+function findDuplicate(nums: number[]): number {
+    for (let index = 0; index < nums.length; index++) {
+        const element =Math.abs(nums[index]);
+        const actualpos = element-1;
+        if (nums[actualpos] < 0) {
+            return element;
+        }
+        nums[actualpos] = -1*nums[actualpos];
+    }
+    return -1;
+};
