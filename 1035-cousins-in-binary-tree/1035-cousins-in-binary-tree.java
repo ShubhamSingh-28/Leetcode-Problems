f11af30dffebc7e24aad75258319826cb7ded552
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-09-18 11:13:18 +0530

    Time: 0 ms (100%), Space: 41.8 MB (10.35%) - LeetHub

---
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root==null){
+            return false;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+         int childcount= 0;
+        while(!q.isEmpty()){
+             int size= q.size();
+            for(int i=0; i<size;i++){
+                TreeNode node = q.poll();
+                int sameParent=0;
+                if(node.left!=null){
+                    if(node.left.val==x || node.left.val==y){
+                      sameParent++;  
+                      childcount++;
+                    }
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    if(node.right.val==x || node.right.val==y){
+                      sameParent++;  
+                      childcount++;
+                    }
+                    q.offer(node.right);
+                }
+                if(sameParent==2){
+                    return false;
+                }
+            }
+            if(childcount==2){
+                return true;
+            }
+            if(childcount==1){
+                return false;
+            }
+        }
+        return false;
+    }
+}
