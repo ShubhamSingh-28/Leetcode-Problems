89faecf4bd7def450654c61b09955970938cdb27
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-10-17 11:02:22 +0530

    Time: 0 ms (100%), Space: 40.6 MB (30.29%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,35 +1,43 @@
 class Solution {
     public int maximumSwap(int num) {
-        // char arr[]= Integer.toString(num).toCharArray();
-        // int n= arr.length;
-        // char max='0';
-        // int m= 0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]>max){
-        //         max=arr[i];
-        //         m=i;
-        //     }
-        // }
-        // if(max==arr[0]){
-        //     return Integer.parseInt(new String(arr)) ;
-        // } 
-        //  char temp=arr[m];
-        // arr[m]=arr[0];
-        // arr[0]=temp;
-        // return Integer.parseInt(new String(arr)) ;
-        int res = num, place = 10, n = num / 10;
-        int maxDigit = num % 10, maxPlace = 1;
-        while (n != 0){
-            int d = n % 10;
-            if (maxDigit > d)
-                res = (num - d * place) + (maxDigit * place) - (maxDigit * maxPlace) + (d * maxPlace);
-            else if (d != maxDigit) {
-                maxDigit = d;
-                maxPlace = place;
+        char arr[]= Integer.toString(num).toCharArray();
+        int n= arr.length;
+        char max=arr[n-1];
+        int m= n-1;
+        int s1=-1;
+        int s2=-1;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>max){
+                max=arr[i];
+                m=i;
+            } else if(arr[i]<max){
+                s1=i;
+                s2=m;
             }
-            place *= 10;
-            n /= 10;
         }
-        return res;  
+
+        if(s1 != -1){
+            char temp=arr[s1];
+            arr[s1]=arr[s2];
+            arr[s2]=temp;
+        } 
+        return Integer.parseInt(new String(arr)) ;
+
+
+
+        // int res = num, place = 10, n = num / 10;
+        // int maxDigit = num % 10, maxPlace = 1;
+        // while (n != 0){
+        //     int d = n % 10;
+        //     if (maxDigit > d)
+        //         res = (num - d * place) + (maxDigit * place) - (maxDigit * maxPlace) + (d * maxPlace);
+        //     else if (d != maxDigit) {
+        //         maxDigit = d;
+        //         maxPlace = place;
+        //     }
+        //     place *= 10;
+        //     n /= 10;
+        // }
+        // return res;  
     }
 }
