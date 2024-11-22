f96b45ee37c8f58896efd8360aa278ad49cc519d
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-11-22 12:28:47 +0530

    Time: 71 ms (22.33%), Space: 74.8 MB (18.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -14,28 +14,34 @@ class Solution {
         int dp[][]= new int[nums.length+1][arr.length+1];
         int n=dp.length;
         int m=dp[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(nums[i-1]==arr[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                } else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return lcs(nums,arr,nums.length,arr.length,dp);
-    }
-
-    public int lcs(int nums[], int arr[],int n,int m, int dp[][]){
-        if(n==0 || m==0){
-            return 0;
-        }
-       if(dp[n-1][m-1]!=-1){
         return dp[n-1][m-1];
-       }
-
-       if(arr[m-1]==nums[n-1]){
-         return dp[n-1][m-1]=lcs(nums,arr,n-1,m-1,dp)+1;
-       } else{
-        int ans1=lcs(nums,arr,n-1,m,dp);
-        int ans2=lcs(nums,arr,n,m-1,dp);
-        return dp[n-1][m-1]= Math.max(ans1,ans2);
-       }
+        //return lcs(nums,arr,nums.length,arr.length,dp);
     }
+
+// memoization code
+    // public int lcs(int nums[], int arr[],int n,int m, int dp[][]){
+    //     if(n==0 || m==0){
+    //         return 0;
+    //     }
+    //    if(dp[n-1][m-1]!=-1){
+    //     return dp[n-1][m-1];
+    //    }
+
+    //    if(arr[m-1]==nums[n-1]){
+    //      return dp[n-1][m-1]=lcs(nums,arr,n-1,m-1,dp)+1;
+    //    } else{
+    //     int ans1=lcs(nums,arr,n-1,m,dp);
+    //     int ans2=lcs(nums,arr,n,m-1,dp);
+    //     return dp[n-1][m-1]= Math.max(ans1,ans2);
+    //    }
+    // }
 }
