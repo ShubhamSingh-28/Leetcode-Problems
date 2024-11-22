809dcc2d352b5c5caa656a33fc8833a3e4931fce
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-11-22 11:14:52 +0530

    Time: 0 ms (100%), Space: 40.8 MB (64.21%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[]=  new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        return robsmemo(nums,nums.length-1,dp);
+    }
+    // public int robs(int[] nums, int n) {
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     return Math.max(robs(nums,n-1),robs(nums,n-2)+nums[n]);
+    // }
+
+    public int robsmemo(int[] nums, int n, int dp[]) {
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]= Math.max(robsmemo(nums,n-1,dp),robsmemo(nums,n-2,dp)+nums[n]);
+    }
+}
