f123c89f9c33fa6f5478688e0608cda132672907
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-11-26 11:24:27 +0530

    Time: 1 ms (100%), Space: 45.6 MB (74.26%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
         boolean isundefeated[]= new boolean[n];
-        Arrays.fill(isundefeated,true);
+        //Arrays.fill(isundefeated,true);
         for(int edge[] : edges){
             int w= edge[0];
             int l= edge[1];
-            isundefeated[l]=false;
+            isundefeated[l]=true;
         }
         int champ = -1;
         int count = 0;
         for(int i=0;i<n;i++){
-            if(isundefeated[i]){
+            if(!isundefeated[i]){
                 champ=i;
                 count++;
             }
