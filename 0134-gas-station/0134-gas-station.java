31c42e3d3a138c9b0b81c654a84b2014a618d89d
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-03-01 10:24:58 +0530

    Time: 2 ms (97.55%), Space: 55.9 MB (84.84%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,17 +1,15 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int max=0;
-        int ans=-1;
-        int sum=0;
-        for(int i=gas.length-1;i>=0;i--){
-            sum = sum +gas[i] -cost[i];
-
-            if(sum>=max){
-                max=sum;
-                ans=i;
-            }
+       int sum=0;
+       int ans=0;
+       int max=0;
+       for(int i=gas.length-1;i>=0;i--){
+        sum=sum+gas[i]-cost[i];
+        if(sum>max){
+            max=sum;
+            ans=i;
         }
-
+       }
         return (sum>=0)?ans:-1;
     }
 }
