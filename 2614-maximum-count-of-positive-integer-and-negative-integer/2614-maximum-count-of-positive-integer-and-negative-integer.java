6c2d698794191c3ab2b3faf5bc14f36244982c5a
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-03-12 10:17:50 +0530

    Time: 0 ms (100%), Space: 45.7 MB (8.64%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,14 +1,49 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        int neg =0;
-        int pos =0;
-        for(int num: nums){
-            if(num>0){
-                pos++;
-            } else if(num<0){
-                neg++;
+    public int positiveIndex(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        int ans=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]<=0){
+                start=mid+1;
+            } else if(nums[mid]>0){
+                end=mid-1;
+                ans=mid;
             }
         }
+         return ans;
+    }
+    public int negetiveIndex(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        int ans=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]<0){
+                start=mid+1;
+            } else if(nums[mid]>=0){
+                end=mid-1;
+                ans=mid;
+            }
+        }
+        return ans;
+    }
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+        int pos=n- positiveIndex(nums);
+        int neg = negetiveIndex(nums);
+
+       // normal approach time-complexity O(N);
+        // int neg =0;
+        // int pos =0;
+        // for(int num: nums){
+        //     if(num>0){
+        //         pos++;
+        //     } else if(num<0){
+        //         neg++;
+        //     }
+        // }
         return Math.max(pos,neg);
     }
 }
