5a06f641fa37720c4f8733cbfebdb45cf185bc4b
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-03-17 09:43:24 +0530

    Time: 6 ms (45.22%), Space: 45 MB (9.04%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -8,14 +8,25 @@ class Solution {
         // return true;
 
         // 2nd approach hashMap tc:-O(n) and sc O(n);
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int n:nums){
-            map.put(n,map.getOrDefault(n,0)+1);
-        }
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int n:nums){
+        //     map.put(n,map.getOrDefault(n,0)+1);
+        // }
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue() %2 != 0) return false;
+        // for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        //     if(entry.getValue() %2 != 0) return false;
+        // }
+        // return true;
+
+        //3rd aproch set tc:-O(n) and sc O(n);
+        Set<Integer> set = new HashSet<>();
+        for(int n:nums){
+            if(set.contains(n)){
+                set.remove(n);
+            }else{
+                set.add(n);
+            }
         }
-        return true;
+        return set.isEmpty();
     }
 }
