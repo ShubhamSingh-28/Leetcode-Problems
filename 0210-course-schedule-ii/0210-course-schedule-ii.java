5f691ca33df2d1a44360e93463e3184c8ca24ec1
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-03-18 10:11:05 +0530

    Time: 5 ms (78.78%), Space: 46.2 MB (27.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,34 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+      List<List<Integer>> adjlist= new ArrayList<>();
+      int indegree[] = new int[numCourses];
+      for (int i = 0; i < numCourses; i++) {
+            adjlist.add(new ArrayList<>());
         }
-
-        for(int n[]:prerequisites){
-            int u=n[1];
-            int v= n[0];
-            adj.get(u).add(v);
+      for(int p[]:prerequisites){
+        int u=p[0];
+        int v=p[1];
+        adjlist.get(v).add(u);
+        indegree[u]++;
+      }
+      Queue<Integer> q = new LinkedList<>();
+      for(int i=0;i<numCourses;i++){
+        if(indegree[i]==0){
+            q.add(i);
         }
-       int []indegree=new int[numCourses];
-        for (int index = 0; index < adj.size(); index++) {
-            for (int a : adj.get(index)) {
-                indegree[a]++;
-            }
-        }
-         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i]==0) {
-                q.offer(i);
-            }
-        }
-        //ArrayList<Integer> l= new ArrayList<>();
-        int ans[]= new int[numCourses];
+      }
+      int ans[]= new int[numCourses];
         int i=0;
-        while (!q.isEmpty()) {
-            int node =q.poll();
-            ans[i++]=node;
-            for (int a : adj.get(node)) {
-                indegree[a]--;
-                if (indegree[a]==0) {
-                    q.offer(a);
-                }
+      while(!q.isEmpty()){
+        int node=q.poll();
+        ans[i++]=node;
+        for(int neigh:adjlist.get(node)){
+            indegree[neigh]--;
+            if(indegree[neigh]==0){
+                q.add(neigh);
             }
         }
-        return i==numCourses?ans:new int[0];
+      }
+      return i==numCourses?ans:new int[0];
     }
 }
