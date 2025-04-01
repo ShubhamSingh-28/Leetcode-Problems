2085fd3f8fa821f1fb30856447dbd5d788a2d136
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-04-01 09:52:04 +0530

    Time: 15 ms (21.61%), Space: 126.5 MB (12.86%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        long dp[]= new long[questions.length];
+        Arrays.fill(dp,-1);
+       return helper(questions,dp,0);
+    }
+    public long helper(int[][] questions,long dp[],int i){
+        if(i>=questions.length){
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        long pick=questions[i][0]+helper(questions,dp,i+questions[i][1]+1);
+        long nopick=helper(questions,dp,i+1);
+       return dp[i]=Math.max(pick,nopick);
+    }
+}
