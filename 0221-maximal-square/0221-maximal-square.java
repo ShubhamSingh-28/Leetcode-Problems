d1a93ee0f0a4334ebc9e2fc5851c1790a2a82c1d
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-04-24 11:55:28 +0530

    Time: 8 ms (71.33%), Space: 60.8 MB (10.7%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,21 +1,33 @@
 class Solution {
+    int res=0;
     public int maximalSquare(char[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
         int dp[][]= new int[m][n];
-        int max=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='1'){
-                    if(i==0 || j==0){
-                        dp[i][j]=1;
-                    } else{
-                        dp[i][j]=Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
-                    }
-                    max=Math.max(max,dp[i][j]);
-                }
-            }
+        for(int[] rows: dp)
+            Arrays.fill(rows, -1);
+
+         sol(matrix,0,0,dp);
+         return res*res;
+    }
+    public int sol(char[][] mat,int i,int j, int[][] dp){
+        if(i<0 || i>=mat.length || j<0 || j>=mat[0].length){
+            return 0;
         }
-        return max*max;
+        if(dp[i][j]!= -1){
+            res=Math.max(res,dp[i][j]);
+            return dp[i][j];
+        }
+        int down=sol(mat,i+1,j,dp);
+        int right=sol(mat,i,j+1,dp);
+        int digonal= sol(mat,i+1,j+1,dp);
+
+        if(mat[i][j]=='1'){
+            dp[i][j]=1+Math.min(Math.min(down,right),digonal);
+            res=Math.max(res,dp[i][j]);
+            return dp[i][j];
+        }else{
+            return dp[i][j]=0;
+        }
     }
 }
