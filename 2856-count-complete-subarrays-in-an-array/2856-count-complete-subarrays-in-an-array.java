2c528306dda9260b9995ba1f1842b8e0192beb1a
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-04-24 11:32:15 +0530

    Time: 7 ms (89.07%), Space: 45.1 MB (47.27%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -2,22 +2,39 @@ class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int res =0;
         int n=nums.length;
-        Set<Integer> set= new HashSet<>();
-        for(int a:nums){
-            set.add(a);
-        }
+        
+        //brute force aproach.
+        // Set<Integer> set= new HashSet<>();
+        // for(int a:nums) set.add(a);
+        // int count=set.size();
+        // for(int i=0;i<n;i++){
+        //     set.clear();
+        //     for(int j=i;j<n;j++){
+        //         set.add(nums[j]);
+        //         if(set.size()==count){
+        //             res+=n-j;
+        //             break;
+        //         }
+        //     }
+        // }
 
+        // sliding window 
+
+        Set<Integer> set= new HashSet<>();
+        for(int a:nums) set.add(a);
         int count=set.size();
+        Map<Integer, Integer> map = new HashMap<>();
+        int j = 0;
         for(int i=0;i<n;i++){
-            set.clear();
-            for(int j=i;j<n;j++){
-                set.add(nums[j]);
-                if(set.size()==count){
-                    res+=n-j;
-                    break;
-                }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            while(map.size()==count){
+                res+=n-i;
+                map.put(nums[j],map.get(nums[j])-1);
+                if(map.get(nums[j])==0) map.remove(nums[j]);
+                j++;
             }
         }
+
         return res;
     }
 }
