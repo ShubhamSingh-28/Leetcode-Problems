78f0231c3eab8d88da711f58bdeb122c94a98eb8
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-08-06 10:39:19 +0530

    Time: 0 ms (100%), Space: 41.1 MB (44.05%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int reverse(int x) {
-        if(x > Integer.MAX_VALUE/2 || x < Integer.MIN_VALUE/2) return 0;
+       // if(x > Integer.MAX_VALUE/2 || x < Integer.MIN_VALUE/2) return 0;
         int a=Math.abs(x);
         int res=0;
         while(a!=0){
