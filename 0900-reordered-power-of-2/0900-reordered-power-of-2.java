5d020bee4eb94417930c99d431b10610da105d6a
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-08-11 09:01:02 +0530

    Time: 1 ms (86.17%), Space: 40.9 MB (47.34%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.java
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -1,23 +1,36 @@
 class Solution {
+    private String helper(int n){
+        String s = Integer.toString(n);
+        char arr[]= s.toCharArray();
+        Arrays.sort(arr);
+        String sortedString = new String(arr);
+        return sortedString;
+    }
     public boolean reorderedPowerOf2(int n) {
-        String s = String.valueOf(n);
-        Map<Character, Integer> mpp = new HashMap<>();
-        
-        for (char c : s.toCharArray()) {
-            mpp.put(c, mpp.getOrDefault(c, 0) + 1);
-        }
+        String s = helper(n);
 
-        for (int i = 0; i < 30; i++) { // max comparison till 30 bits
-            int x = 1 << i; // left shift to get multiple power of 2
-            String ss = String.valueOf(x);
-            Map<Character, Integer> fnk = new HashMap<>();
-            for (char c : ss.toCharArray()) {
-                fnk.put(c, fnk.getOrDefault(c, 0) + 1);
-            }
-            if (fnk.equals(mpp)) {
-                return true;
-            }
+        for(int i=0;i<=29;i++){
+            if(s.equals(helper(1<<i))) return true;
         }
         return false;
+        // String s = String.valueOf(n);
+        // Map<Character, Integer> mpp = new HashMap<>();
+        
+        // for (char c : s.toCharArray()) {
+        //     mpp.put(c, mpp.getOrDefault(c, 0) + 1);
+        // }
+
+        // for (int i = 0; i < 30; i++) { // max comparison till 30 bits
+        //     int x = 1 << i; // left shift to get multiple power of 2
+        //     String ss = String.valueOf(x);
+        //     Map<Character, Integer> fnk = new HashMap<>();
+        //     for (char c : ss.toCharArray()) {
+        //         fnk.put(c, fnk.getOrDefault(c, 0) + 1);
+        //     }
+        //     if (fnk.equals(mpp)) {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
