d64b3e69ac5d960524e77ed0da9687aa3900240f
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-08-18 11:25:49 +0530

    Time: 0 ms (100%), Space: 44.9 MB (51.13%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -16,19 +16,18 @@
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         // we use array because if we use normal variable we will always get 0.
-        int res[]= new int[1];
-
-        int a= depth(root, res);
-        return res[0] ;
+        int res[]=new int[1];
+        depth(root,res);
+        return res[0];
     }
     public int depth(TreeNode root, int [] res){
-        if(root==null){
-            return 0;
-        }
-        int lh= depth(root.left,res);
-        int rh=depth(root.right, res);
+      if(root==null){
+        return 0;
+      }
+      int lh = depth(root.left,res);
+      int rh = depth(root.right, res);
 
-        res[0]= Math.max(res[0], lh+rh);
-        return (1+Math.max(lh,rh));
+      res[0]= Math.max(res[0], lh+rh);
+      return (1+Math.max(lh,rh));
     }
 }
